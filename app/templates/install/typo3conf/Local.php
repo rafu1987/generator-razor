bff99b729ca244d30c1dc4353165a30ad51045b6
Raphael Zschorsch <rafu1987@gmail.com>
2015-09-02 17:46:54 +0200

    [BUGFIX] Local.php

--- app/templates/install/typo3conf/Local.php
+++ app/templates/install/typo3conf/Local.php
@@ -1,15 +1,6 @@
 <?php
 
-// Config
-$confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['razor']);
-
-if ($_SERVER['SERVER_NAME'] == trim($confArr['basedomain_local']) || getenv('APP_ENV') == 'local') {
-	$GLOBALS['TYPO3_CONF_VARS'] = array(
-		'MAIL' => array(
-			'transport' => '###TRANSPORT###',
-			'transport_smtp_password' => '###SMTP_PASS###',
-			'transport_smtp_server' => '###SMTP_SERVER###',
-			'transport_smtp_username' => '###SMTP_USER###'
-		)
-	);
-}
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = '###TRANSPORT###';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_password'] = '###SMTP_PASS###';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = '###SMTP_SERVER###';
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_username'] = '###SMTP_USER###';
