90698cf18932ca327748dff83688f49c32b9d14a
Raphael Zschorsch <rafu1987@gmail.com>
2020-03-11 17:10:28 +0100

    [TASK] Bugfix

--- app/templates/95/typo3conf/LocalConfiguration.php
+++ app/templates/95/typo3conf/LocalConfiguration.php
@@ -73,6 +73,11 @@ return [
         'transport_smtp_username' => '',
     ],
     'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'staticfilecache' => [],
+            ],
+        ],
         'devIPmask' => '*',
         'displayErrors' => 0,
         'encryptionKey' => '8fde7d2ba1d52852be87d2349b5800b0f2fc871bce0e96f5d9db0a3c04ce31310a5d815325c87b172fb2f724cd563736',
